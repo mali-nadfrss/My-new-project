1b66d32bb9f5847490c2e9add73f8f06bf925f94
Naveen reddy Mali <Fe229@NSLHUB.COM>
2020-10-08 16:18:39 +0530

    k most frequent

---
+++ src/main/java/com/mali/ds/Queues/KMostFrequentQImpl.java
@@ -0,0 +1,39 @@
+package com.mali.ds.Queues;
+
+import java.util.*;
+
+public class KMostFrequentQImpl {
+
+    static void logic(int[] arr, int k) {
+
+        Map<Integer, Integer> m = new HashMap<>();
+        for (int i :
+                arr) {
+            if (m.containsKey(i)) {
+                m.put(i, m.get(i) + 1);
+            } else {
+                m.put(i, 1);
+            }
+        }
+
+        Queue<Map.Entry<Integer,Integer>> q = new PriorityQueue<>((a,b) ->
+                a.getValue().equals(b.getValue()) ? b.getKey().compareTo(a.getKey()) :
+                b.getValue().compareTo(a.getValue()));
+        for (Map.Entry<Integer,Integer> e: m.entrySet()
+             ) {
+            q.add(e);
+        }
+
+        for (int i = 0; i < k; i++) {
+            System.out.println(Objects.requireNonNull(q.poll()).getKey());
+        }
+
+    }
+
+
+    public static void main(String args[]) {
+
+        int[] arr = {1, 2, 3, 5, 4, 4, 5,1,1,1};
+        logic(arr, 3);
+    }
+}
