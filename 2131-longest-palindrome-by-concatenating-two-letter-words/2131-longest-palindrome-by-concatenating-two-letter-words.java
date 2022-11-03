e6cac1f65cbc0e6c8340e05a4ca24d0d5b8a7a41
mali naveen reddy <42131241+mali-nadfrss@users.noreply.github.com>
2022-11-03 12:27:56 +0530

    Time: 139 ms (61.67%), Space: 104.4 MB (65.74%) - LeetHub

---
+++ 2131-longest-palindrome-by-concatenating-two-letter-words/2131-longest-palindrome-by-concatenating-two-letter-words.java
@@ -0,0 +1,69 @@
+class Solution {
+    public int longestPalindrome(String[] words) {
+        
+        Map<String, Integer> map = new HashMap<>();
+        
+        Map<String, Integer> palins = new HashMap<>();
+        int ans = 0;
+        
+        for(String word : words){
+            map.put(word, map.getOrDefault(word, 0)+1);
+        }
+        
+        int lp = 0;
+        for(Map.Entry<String , Integer> e : map.entrySet()){
+            
+            String s = e.getKey();
+            String r = reverse(s);
+             if(isPalin(s)){
+                 if(map.get(s) % 2  == 0)
+                ans +=  s.length()*map.get(s); 
+                 else if(map.get(s) > 1 ){
+                     ans +=  s.length()*(map.get(s)-1); 
+                     lp = Math.max(lp, s.length());
+                 }
+                 else
+                     lp = Math.max(lp, s.length()*map.get(s));
+                 
+             }
+            else if(palins.get(r) == null && map.get(r) != null){
+                palins.put(s, Math.min(map.get(s), map.get(r)));
+            } 
+                
+    
+            
+        }
+        
+        
+        
+        for(Map.Entry<String , Integer> e : palins.entrySet()){
+            
+            ans += e.getKey().length()*e.getValue()*2;
+            
+        }
+        
+        return ans+lp;
+        
+        
+    }
+    
+    String reverse(String a){
+        StringBuilder sb = new StringBuilder();
+        return sb.append(a).reverse().toString();
+    }
+    
+    boolean isPalin(String s ){
+        
+        int len = s.length();
+        
+        for(int i = 0 ; i <= len/2 ; i++){
+            if(s.charAt(i) != s.charAt(len-1-i)){
+                return false;
+            }
+        }
+        return true;
+    }
+    
+    //["dd","aa","bb","dd","aa","dd","bb","dd","aa","cc","bb","cc","dd","cc"]
+    
+}
