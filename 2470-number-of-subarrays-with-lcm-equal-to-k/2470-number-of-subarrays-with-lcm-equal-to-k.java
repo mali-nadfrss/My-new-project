9068eaa5e9e4b5cfa06506425065afdadc41ce62
mali naveen reddy <42131241+mali-nadfrss@users.noreply.github.com>
2022-11-13 10:15:03 +0530

    Time: 2 ms (100.00%), Space: 42.8 MB (25.00%) - LeetHub

--- 2470-number-of-subarrays-with-lcm-equal-to-k/2470-number-of-subarrays-with-lcm-equal-to-k.java
+++ 2470-number-of-subarrays-with-lcm-equal-to-k/2470-number-of-subarrays-with-lcm-equal-to-k.java
@@ -1,32 +1,37 @@
 class Solution {
 
-    int gcd(int a, int b)
-{
-    if (a == 0)
-        return b;
-    return gcd(b % a, a);
-}
- 
-// method to return LCM of two numbers
- int LCM(int a, int b)
-{
-    return (a / gcd(a, b)) * b;
-}
-
-public int subarrayLCM(int[] nums, int k) {
-    int ans = 0;
-    for (int i = 0; i < nums.length; i++) {
-        int currLCM = nums[i];
-        if(currLCM == k) // if element is equal to k, increment answer
-            ans++;
-        for (int j = i + 1; j < nums.length; j++) {
-            if(nums[j] > k) // if nums[j] > k lcm can never be equal to k for this subarray
-                break;
-            currLCM = LCM(nums[j], currLCM);
-            if (currLCM == k)
-                ans++;
+    public int subarrayLCM(int[] nums, int k) {
+        int ans = 0;
+        int start = 0;
+        int prev = -1;
+        
+        for (int i = 0; i < nums.length; i++) {
+        
+            // case if num = k
+            if (nums[i] == k) ans++;
+            
+            //check if num has num is a multiple of k
+            if (k % nums[i] == 0) {
+                //check if lcm of current and prev lcm value is k
+                if (prev != -1 && lcm(nums[i], prev) == k) {
+                    // we sub arrays so just count from start to current
+                    ans += i - start;
+                }
+                prev = nums[i];
+            } else {
+                //if its not a multiple simply start from next
+                start = i + 1;
+            }
         }
+        return ans;
     }
-    return ans;
-}
+
+    int gcd(int a, int b) {
+        if (a == 0) return b;
+        return gcd(b % a, a);
+    }
+
+    int lcm(int a, int b) {
+        return (a / gcd(a, b)) * b;
+    }
 }
