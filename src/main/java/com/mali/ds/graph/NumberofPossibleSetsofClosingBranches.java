3c38a8654fbd89b96194b70b4c9ff7b3290471af
Naveen Mali <nmali@modeln.com>
2023-12-15 12:11:09 +0530

    weeekend practice

---
+++ src/main/java/com/mali/ds/graph/NumberofPossibleSetsofClosingBranches.java
@@ -0,0 +1,17 @@
+package com.mali.ds.graph;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+/* https://leetcode.com/contest/biweekly-contest-119/problems/number-of-possible-sets-of-closing-branches/ */
+public class NumberofPossibleSetsofClosingBranches {
+
+    public int numberOfSets(int n, int maxDistance, int[][] roads) {
+
+        Queue<int[]> q = new LinkedList<>();
+        q.add(new int[]{0, 0});
+        
+
+    }
+
+}
